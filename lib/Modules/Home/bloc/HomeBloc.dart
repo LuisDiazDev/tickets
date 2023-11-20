400ae64aa5b0e9c3fd42eeaa67763707c606ac65
luisdiaz <luiscdo1997@gmail.com>
2023-11-20 13:47:01 -0400

    update

--- lib/Modules/Home/bloc/HomeBloc.dart
+++ lib/Modules/Home/bloc/HomeBloc.dart
@@ -4,6 +4,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:qr_flutter/qr_flutter.dart';
 import 'package:share_plus/share_plus.dart';
+import '../../../Core/utils/rand.dart';
 import '../../../Data/Provider/TicketProvider.dart';
 import '../../Alerts/AlertCubit.dart';
 import 'HomeEvents.dart';
@@ -20,7 +21,26 @@ class HomeBloc extends Bloc<HomeEvent, HomeState> {
         emit(state.copyWith(load: true));
         var data = await provider.allTickets();
         var profiles = await provider.allProfiles();
-        emit(state.copyWith(load: false, tickets: data,profiles: profiles));
+        emit(state.copyWith(load: false, tickets: data, profiles: profiles));
+      },
+    );
+
+    on<GeneratedTickets>(
+      (event, emit) async {
+
+        var cant = double.tryParse(event.cant) ?? 1;
+
+        for (int i = 0; i < cant; i++){
+          var name =  generateUser();
+          while(state.tickets.where((t) => t.name == name) != null){
+            name =  generateUser();
+          }
+
+          //generated
+        }
+
+        var data = await provider.allTickets();
+        emit(state.copyWith(load: false, tickets: data));
       },
     );
 
@@ -38,7 +58,9 @@ class HomeBloc extends Bloc<HomeEvent, HomeState> {
           .create(); // Create a file to store the generated image
       var bytes = image!.buffer.asUint8List(); // Get the image bytes
       await file.writeAsBytes(bytes); // Write the image bytes to the file
-      await Share.shareXFiles([XFile(file.path,mimeType: ".png")],text: "${event.user} ${event.password}"); // Share the generated image using the share_plus package
+      await Share.shareXFiles([XFile(file.path, mimeType: ".png")],
+          text:
+              "${event.user} ${event.password}"); // Share the generated image using the share_plus package
     });
   }
 
