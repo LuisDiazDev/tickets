55f95c65e2c3e0b1e8e13ca75cf326236d8c2fc3
Hector Oliveros <hector.oliveros.leon@gmail.com>
2024-01-23 12:33:14 -0300

    add sentry support

--- lib/main.dart
+++ lib/main.dart
@@ -9,12 +9,22 @@ import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:path_provider/path_provider.dart';
 
 import 'package:firebase_core/firebase_core.dart';
+import 'package:sentry_flutter/sentry_flutter.dart';
 import 'firebase_options.dart';
 
 import 'Data/Provider/restApiProvider.dart';
 import 'MyApp.dart';
 
 Future<void> main() async {
+  await SentryFlutter.init(
+        (options) {
+      options.dsn = 'https://d2ec9772122589b058b8bfb4460904e7@o878781.ingest.sentry.io/4506621199319040';
+      // Set tracesSampleRate to 1.0 to capture 100% of transactions for performance monitoring.
+      // We recommend adjusting this value in production.
+      options.tracesSampleRate = 1.0;
+    },
+    appRunner: () => runApp(MyApp()),
+  );
   WidgetsFlutterBinding.ensureInitialized();
   HttpOverrides.global = MyHttpOverrides();
   await Hive.initFlutter();
